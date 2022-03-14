6755fc2bea0c8bb21b5cc1eb512e69d60126d8fd
Mohd Imran <mohd78696321@gmail.com>
2022-03-14 18:15:41 +0530

    Update Signup.js

--- frontend/src/components/Signup.js
+++ frontend/src/components/Signup.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { useHistory } from "react-router-dom/cjs/react-router-dom.min";
+import { useHistory } from "react-router-dom";
 
 const Signup = ({ showAlert }) => {
   const [credentials, setCredentials] = useState({
